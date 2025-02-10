055225f07b095d1800c76c7cc312b97a1bd68f72
marianocastillo <marianocastillo08@gmail.com>
2025-02-09 21:29:34 -0400

    Parte2

--- src/App.vue
+++ src/App.vue
@@ -7,32 +7,34 @@
   <div class="container login-container " style="width: 500px;">
     <div class="row">
       <div class="col-md-6 ads ">
-        <span id="fl"><img src="./assets/logo.png" class="<img img-fluid "></span>
+        <span id="fl"><img src="./assets/logo.png" class="<img img-fluid"></span>
       </div>
       <div class="col-md-6 login-form">
-        <div class="profile-img">
-          <img src="./assets/logo.png"  style="width: 150px;">
-        </div>
        
-        <form>
-          <div class="form-group">
-            <input type="text" class="form-control m-2" name="username" placeholder="Usuario">
+          <div class="profile-img m-3">
+            <img src="./assets/logo.png" style="width: 200px;">
           </div>
-          <div class="form-group">
-            <input type="text" class="form-control m-2" name="password" placeholder="Contraseña">
+
+          <form>
+          <div class=" mb-3 input-group">
+            <span class="input-group-text"><i class="fas fa-user"></i></span>
+            <input type="text" class="form-control" name="username" placeholder="Usuario">
           </div>
-          <div class="form-group">
-            <button type="button" class="m-2">Iniciar</button>
+          <div class="input-group">
+            <span class="input-group-text"><i class="fas fa-lock"></i></span>
+            <input type="text" class="form-control" name="password" placeholder="Contraseña">
           </div>
+          <div class="d-flex justify-content-center align-items-center">
+            <button type="button" class="button-7 small-button m-4 pb-2 ">Iniciar</button>
+          </div>
+
           <div class="form-group forget-password m-2 ">
-            <a href="#">Olvidaste la contraseña</a>
+            <a href="#">¿Olvidaste la contraseña?</a>
           </div>
         </form>
       </div>
     </div>
   </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+  
 </template>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
-import './assets/main.css'
+import './assets/main.css';
+import './assets/user.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import UserInterface from './UserInterface.vue';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+
+
+const routes = [
+  { path: '/', component: Login },
+  { path: '/userinterface', component: UserInterface }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
 
 
 
 
 createApp(App).mount('#app')
+createApp(UserInterface).mount('#userinterface')
